0dc1087ba7f0384840640995fee3fa83a47933aa
shahithkumar <shahithu2004@gmail.com>
2026-01-29 20:08:16 +0530

    Add migrations to build script

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -12,4 +12,7 @@ python3.12 -m pip install -r requirements.txt
 # Collect static files
 python3.12 manage.py collectstatic --noinput --clear
 
+# Run migrations
+python3.12 manage.py migrate --noinput
+
 echo "BUILD END"
