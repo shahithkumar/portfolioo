d64ba2250e7c0e0b04f6f9343b98be1079e5d943
shahithkumar <shahithu2004@gmail.com>
2026-01-29 20:12:25 +0530

    Fix routing and add home page

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', lambda r: HttpResponse("<h1>Portfolio Backend is Running!</h1><p>Visit <a href='/api/projects/'>/api/projects/</a> to see data.</p>")),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
